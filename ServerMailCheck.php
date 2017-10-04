6cfcea7a6d6dedccd9f296f89d389a2b9fe63a73
spraveenitpro <praveen.selvasekaran@automattic.com>
2017-10-04 22:41:05 +0530

    Completed OOPS code

---
+++ ServerMailCheck.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+Plugin Name: Server-Mail-Check
+Plugin URI: http://praveen.blog
+Description: Check if the emails are shooting as expected from your hosting server.
+Version: 1.0
+Author: praveen
+Author URI: http://praveen.blog
+License: GPL2
+*/
+
+class ServerMailCheck
+{
+
+    public static function render_dashboard_page()
+    {
+        add_action('admin_menu', 'checkmailmenu');
+
+        function checkmailmenu()
+        {
+            add_dashboard_page('Check Email', 'Check Email', 'activate_plugins', 'servermailcheck', 'render_email_form');
+        }
+
+
+        function render_email_form()
+        {
+            echo "<h2>Server Mail Check</h2>";
+            $current_user = wp_get_current_user();
+            ?>
+            <form action="" method="post">
+                <label>Enter your email address:</label> <input id="email" type="email" name="yourmail" value="<?php echo $current_user->user_email; ?>" disabled="disabled">
+                <input type="submit" name="submit" value="Test">
+            </form>
+
+        <?php
+
+            if (isset($_POST["submit"])) {
+                $headers = array('Content-Type: text/html; charset=UTF-8');
+                $to = $current_user->user_email;
+                $title = "Test email from " . get_bloginfo("url");
+                $body = "This test email proves that your WordPress installation can send emails";
+                $mailresult = wp_mail($to, $title, $body, $headers);
+                if ($mailresult) {
+                    echo 'Email Works!!.';
+                } else {
+                    echo 'There seems to be some issue with the sending out of emails!';
+                }
+            }
+        }
+    }
+
+
+}
+
+ServerMailCheck::render_dashboard_page();
+
+?>
---
+++ readme.txt
@@ -0,0 +1,30 @@
+readme.=== Mail Check ===
+Tags: Email Check
+Contributors: Praveen
+Requires at least: 4.0.0
+Tested up to: 4.3.0
+
+== Description ==
+
+This plugin will send an email out to the administrator of the WordPress site to confirm
+and check if the server is able to send out an email
+
+
+In future this plugin will also provide a whole suite of checks for other things.
+
+== Installation ==
+
+1. Upload to your plugins folder, usually `wp-content/plugins/`
+2. Activate the plugin on the plugin screen.
+
+== Screenshots ==
+
+1. This shows the plugin in action.  When you activate it you get a new link under the Dashboard link
+on the top left side, proceed to click the `Test` button to send out the email.
+
+== Changelog ==
+
+
+
+= v 1.0 =
+* Initial release with checks for the PHP and MySQL versions we will likely target for WordPress 3.2
